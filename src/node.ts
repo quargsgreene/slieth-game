ec5757ab01213ee3ae706a0312ad4f4bd9d4c4e2
quargsgreene <quargsgreene@gmail.com>
2025-08-15 00:24:06 +0000

    implemented node getters

--- src/node.ts
+++ src/node.ts
@@ -5,9 +5,76 @@ export interface Node
 	text: string[],
 	images: string[],
 	audio: string[],
-	puzzles: [],
+	puzzles: string[],
 	apiData: [],
-	value: number
+	value: number,
+	active:boolean
 
+}
+
+
+
+function getNodeId()
+{
+
+	return this.id
+
+}
+
+
+function getNodeText()
+{
+
+	return this.text
+
+}
+
+
+function getImages()
+{
+
+	return this.images
+
+}
+
+function getAudio()
+{
+
+	return this.audio
+
+}
+
+
+function getPuzzles()
+{
+
+	return this.puzzles
+
+}
+
+
+function getApiData()
+{
+
+	return this.apiData
+
+}
+
+
+function getValue()
+
+{
+
+
+	return this.value
+
+}
+
+
+function isActive()
+
+{
+
+	return this.active
 
 }
