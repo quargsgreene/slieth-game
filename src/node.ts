6d2a490a6e9895d1da1bc6f5a2a64cf246171c43
quargsgreene <quargsgreene@gmail.com>
2025-08-16 02:35:03 +0000

    preliminary game progress heap implementation

--- src/node.ts
+++ src/node.ts
@@ -16,97 +16,73 @@ export interface Node
 
 function getNodeId(this:Node)
 {
-
 	return this.id
-
 }
 
 
 function getNodeText(this:Node)
 {
-
 	return this.text
-
 }
 
 
 function getImages(this:Node)
 {
-
 	return this.images
-
 }
 
 function getAudio(this:Node)
 {
-
 	return this.audio
-
 }
 
 
 function getPuzzles(this:Node)
 {
-
 	return this.puzzles
-
 }
 
 
 function getApiData(this:Node)
 {
-
 	return this.apiData
-
 }
 
 
 function getValue(this:Node)
 
 {
-
-
 	return this.value
-
 }
 
 
 function isActive(this:Node)
 
 {
-
 	return this.active
-
 }
 
 
 function setId(this:Node, newId:number)
 {
 	this.id = newId
-
 }
 
 function setText(this:Node, newText:string[])
 {
-
 	this.text = newText
-
 }
 
 
 function setImages(this:Node, newImages:string[])
 {
-
 	this.images = newImages
-
 }
 
 
 function setAudio(this:Node, newAudio:string[])
 {
-
 	this.audio = newAudio
-
 }
 
 
@@ -120,29 +96,14 @@ function setPuzzles(this:Node, newPuzzles:string[])
 function setApiData(this:Node, newApiData:[])
 
 {
-
 	this.apiData = newApiData
-
-}
-
-
-function setValue(this:Node, newValue:number)
-
-{
-
-	this.value = newValue
-
 }
 
 
 function toggleActive(this:Node, activityState:boolean)
 
 {
-
-
 	this.active = activityState
-
-
 }
 
 
--- src/tree.ts
+++ src/tree.ts
@@ -2,14 +2,12 @@ import type {Node} from './node.ts'
 
 export type Routes = '/' | 'active' | 'inactive'
 
-
 export interface GameTree
 {
 	game_id:string,
 	nodes:Node[],
 	active:boolean,
 	root:Node
-
 }
 
 export type GameTreeType = GameTree[]
@@ -24,78 +22,147 @@ export interface GameState
 
 export enum LocalStorageKey
 {
-
 	GAME_STATE = 'GAME_STATE',
-
 }
 
 //TODO: insert, delete, heapify, extract min
 
-function insertNode(node:Node)
+function insertNode(this:GameTree, node:Node): void
 {
+	let nodes: Node[] = this.nodes
+	nodes.push(node)
+	let size: number = nodes.length
+	let parent:Node = nodes[Math.floor((size - 2)/2)]
 
-	//
-
+	if(node.value < parent.value)
+	{
+		this.upHeap(node)
+	}
 }
 
 
-function deleteNode(node:Node)
+function deleteNode(this:GameTree, node:Node): Node
 
 {
+	let nodes:Node[] = this.nodes
+	let nodeIndex: number = nodes.indexOf(node)
+	let placeholderNode:Node = new Node(0, Infinity, false)
+	let deleted:Node = nodes.splice(nodeIndex, 1, placeholderNode)
+	let deletedNodeParent:Node = this.nodes[Math.floor((nodeIndex - 1)/2)]
+	this.downHeap(placeholderNode)
+	delete placeholderNode
+	return deleted
+}
 
+function extractMin(this:GameTree): Node
+{
+	let removed:Node  =  this.nodes.shift()
+	let nodes:Node[] = this.nodes
+	let placeholderNode:Node = new Node(0, Infinity, false)
+	this.nodes.unshift(placeholderNode)
+	this.downHeap(placeholderNode)
+	delete placeholderNode
+	return removed	
+}
 
-	//
+function upHeap(this:GameTree, node:Node):void
+{
+	let currentNodeIndex: number = nodes.indexOf(node)
+	let currentParent: Node = this.nodes[Math.floor((nodeIndex - 1)/2)]
+	let currentParentIndex: number = Math.floor((nodeIndex - 1)/2)
+	let nodes: Node[] = this.nodes
+	let currentNode: Node = node
+
+	while(node.value < currentParent.value && currentNodeIndex > 0)
+	{
+		let temp:Node = currentNode
+		nodes[currentNodeIndex] = currentParent
+		nodes[currentParentIndex] = temp
+		currentNodeIndex = currentParentIndex
+		currentNode = nodes[currentNodeIndex]
+		currentParentIndex = Math.floor((currentParentIndex - 1)/2)
+		currentParent = nodes[currentParentIndex]
+	}
+
+	if(nodes[0] != nodes.root){
+		this.root = nodes[0]
+	}
 
 }
 
-
-
-function extractMin()
+function downHeap(this:GameTree, node:Node): void
 {
+	let currentNodeIndex: number = nodes.indexOf(node)
+	let currentNode: Node = node
+	let nodes: Node[] = this.nodes
+	let currentLeftChild: Node = nodes[2*currentNodeIndex + 1]
+	let currentRightChild: Node = nodes[2*currentNodeIndex + 2]
 
-	//
+	while(currentNodeIndex < nodes.length)
+	{
+		if(currentNode.value >= currentLeftChild.value)
+		{
+			let temp:Node = currentNode
+			nodes[currentNodeIndex] = currentLeftChild
+			nodes[2*currentNodeIndex + 1] = temp
+			currentNodeIndex = 2*currentNodeIndex + 1
+			currentNode = nodes[currentNodeIndex]
+			currentLeftChild = nodes[2*currentNodeIndex + 1]
 
+		}else if (currentNode.value >= currentRightChild.value){
+			let temp:Node = currentNode
+			nodes[currentNodeIndex] = currentRightChild
+			nodes[2*currentNodeIndex + 2] = temp
+			currentNodeIndex = 2*currentNodeIndex + 2
+			currentNode = nodes[currentNodeIndex]
+			currentRightChild = nodes[2*currentNodeIndex + 2]
+		} else {
+			break
+		}
+	}
 }
 
 
-function heapify()
+function getNodes(this:GameTree): Node[]
 {
-
-
-	//
-
-}
-
-
-
-function getNodes(this:GameTree)
-{
-
 	return this.nodes
-
 }
 
 
-function getId(this:GameTree)
+function getId(this:GameTree): number
 {
-
 	return this.game_id
-
 }
 
-function getRoot(this:GameTree)
+function getRoot(this:GameTree):Node
 {
-
 	return this.root
-
 }
 
-
-function isActive(this:GameTree)
+function setNodeValue(this:GameTree, node:Node, newValue:number):void
 {
+	node.value = newValue
+	let nodeIndex:number = this.indexOf(node)
+	let nodes:Node[] = this.nodes
+	let leftChild:Node = nodes[2*nodeIndex + 1]
+	let rightChild:Node = nodes[2*nodeIndex + 2]
+	let parent:Node = nodes[Math.floor(nodeIndex -1)/2]
 
+	if(node.value >= leftChild.value || node.value >= rightChild.value)
+	{
+		this.downHeap(node)		
+
+	}
+
+	if(node.value < parent.value)
+	{
+		this.upHeap(node)
+	}
+}	
+
+function isActive(this:GameTree):boolean
+{
 	return this.active
-
 }
 
 
@@ -103,23 +170,16 @@ function loadAppStateFromLocalStorage(): GameState
 {
 	const stringifiedJSON: string | null = window.localStorage.getItem(LocalStorageKey.GAME_STATE,
 									  )
-
-
 	if( typeof stringifiedJSON === 'string')
 	{
 		const Loaded: GameState = JSON.parse(stringifiedJSON)
 		return Loaded
-	
 	}
 
-		
 	const NewGameState: GameState = {
 
 		gameTree: [],
 	}
 
-	return NewGameState
-	
+	return NewGameState	
 }	
-
-
